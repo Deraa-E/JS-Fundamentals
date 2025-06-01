f26b9dc75554f2dc581901e90a68e6fbbf84dded
chideraa <chideraaemeribe@gmail.com>
2025-06-01 01:32:53 +0100

    c again

---
+++ 7-multi_c.js
@@ -0,0 +1,9 @@
+const times = Number(process.argv[2]);
+
+if (!times) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < times; i++) {
+    console.log('C is fun');
+  }
+}
